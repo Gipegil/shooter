14caaf2a5be704f539622c2233b92d9e6893ad8f
Gipegil <103821087+Gipegil@users.noreply.github.com>
2022-04-23 11:58:13 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,14 @@
-while 1:
-    if int(input()) % 2 == 0:
-        print('Четное')
+if int(input()) % 2 == 0:
+    print('Четное')
+else:
+    print('Нечетное')
+        
+a = ['a','e','i','o','u']
+b = input()
+if b != 'y':
+    if b not in a:
+        print('Согл.')
     else:
-        print('Нечетное')
+        print('Глас.')
+else:
+    print('Глас. и Согл.')
