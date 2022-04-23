3c455fb6faf996cacb42669154b697875a090e19
Gipegil <103821087+Gipegil@users.noreply.github.com>
2022-04-23 12:12:13 +0300

    Update test4.py

--- test4.py
+++ test4.py
@@ -6,3 +6,12 @@ if pasport(int(input())):
     print('Паспорт есть')
 else:
     print("Паспорта нет")
+
+
+def dostavka(a):
+    b = 0
+    if a >= 1:
+        b += 10.95
+    b += (a-1)*2.95
+    return b
+print('$'+str(dostavka(int(input()))))
